c3aae7c75a704ce43c6b921cbb82781dfa3a6274
Emmanuel Kyei Nketia <eknketia001@st.ug.edu.gh>
2022-04-20 13:46:58 +0000

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Emmanuel K. Nketia on 4/20/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
